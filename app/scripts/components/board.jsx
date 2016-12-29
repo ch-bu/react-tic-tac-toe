d8d0dc77415ab170f424534b90bb858fad58c594
Christian Burkhart <burkhart.christian@gmail.com>
2016-12-29 11:47:39 +0100

    calculate winner

--- app/scripts/components/board.jsx
+++ app/scripts/components/board.jsx
@@ -16,7 +16,15 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    // Do we have a winner yet?
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -46,6 +54,26 @@ class Board extends React.Component {
       squares: squares,
       xIsNext: !this.state.xIsNext,
     });
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 }
 
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,7 @@ import Game from './components/game.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 ReactDOM.render(
   <Game/>,
   document.getElementById('container')
